770c7e3d8675413fe9a8b06d494139357bf9a8ff
lmimoh <dlalsgns9039@gmail.com>
2022-12-24 03:51:14 +0900

    feat : Navigation foldMenu 구현

--- client/src/Components/Share/Navigation.js
+++ client/src/Components/Share/Navigation.js
@@ -4,11 +4,13 @@ import logo from "../../Assets/logo.png";
 import menu from "../../Assets/menu.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { searchData } from "../../Assets/searchData";
 
 const StyledNav = styled.header`
   width: 100%;
-  height: 48px;
+  min-width: 1240px;
+  height: 47px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -19,32 +21,78 @@ const StyledNav = styled.header`
   border-bottom: 1px solid #dadbdc;
   box-shadow: 0 4px 4px -4px #e2e3e4;
 
+  button:nth-of-type(1) {
+    width: 48px;
+    height: 100%;
+    border: 0px;
+    margin: 0px;
+    padding: 0px;
+
+    :hover {
+      background-color: #e0e3e5;
+    }
+
+    img {
+      margin: 5px 0px 0px 0px;
+    }
+  }
+
+  button:nth-of-type(2) {
+    width: 169px;
+    height: 100%;
+    border: 0px;
+    margin: 0px;
+    padding: 0px;
+
+    :hover {
+      background-color: #e0e3e5;
+    }
+
+    img {
+      margin: 5px 0px 0px 0px;
+    }
+  }
+
   nav {
     height: 26px;
   }
 
-  img:last-child {
-    margin: 0px 10px 0px 20px;
-  }
-
   a {
-    color: black;
     text-decoration: none;
     margin: 0px 15px 0px 15px;
     font-size: 12px;
     font-weight: 400;
     color: #696969;
   }
+
+  button:nth-of-type(3) {
+    margin: 0px 5px 0px 5px;
+  }
+
+  nav {
+    display: flex;
+
+    div {
+      height: 30px;
+
+      :hover {
+        border-radius: 30px;
+        background-color: #e0e3e5;
+        cursor: pointer;
+      }
+    }
+  }
 `;
 
 const StyledSearch = styled.div`
   width: 665px;
-  height: 33px;
+  height: 33px !important;
   display: flex;
   align-items: center;
   border: 1px solid ${({ focused }) => (focused ? "#6BBBF7" : "#BABFC4")};
   box-shadow: ${({ focused }) => (focused ? "0px 0px 4px 4px #d3e5f2" : "")};
   border-radius: 2px;
+  margin: 0px 0px 0px 10px;
 
   svg {
     color: #838c95;
@@ -58,54 +106,177 @@ const StyledSearch = styled.div`
 `;
 
 const StyledDiv = styled.div`
-  display: ${({ focused }) => (focused ? "flex" : "none")};
+  display: flex;
   justify-content: center;
   width: 100%;
-  height: 250px;
+  min-width: 1240px;
+  height: 170px;
   position: fixed;
   top: 50px;
+`;
 
-  div {
-    width: 665px;
-    height: 100%;
-    border: 1px solid #babfc4;
-    border-radius: 7px;
-    margin-left: 436px;
-    box-shadow: 0 4px 4px -4px #e2e3e4;
-    background-color: white;
+const StyledHistory = styled.div`
+  visibility: ${({ focused }) => (focused ? "visible" : "hidden")};
+  position: relative;
+  top: -2px;
+  left: 39px;
+  width: 667px;
+  height: 100%;
+  border: 1px solid #babfc4;
+  border-radius: 7px;
+  box-shadow: 0 4px 4px -4px #e2e3e4;
+  background-color: white;
+
+  ul {
+    margin: 10px 0px 0px 10px;
+    padding: 0px;
+    width: 100%;
+    list-style-type: none;
+    color: gray;
+    font-size: 12px;
+
+    li:nth-child(n) {
+      margin: 0px 0px 5px 0px;
+    }
   }
+
+  div:first-child {
+    width: 100%;
+    height: 70%;
+    display: flex;
+  }
+
+  div:last-child {
+    width: 100%;
+    height: 30%;
+    border-top: 1px solid #babfc4;
+
+    button {
+      float: right;
+      margin: 13px 10px 0px 0px;
+    }
+  }
+
+  div span {
+    font-size: 14px;
+    font-weight: 400;
+    color: black;
+    margin: 0px 10px 0px 0px;
+  }
+`;
+
+const StyledButton = styled.button`
+  font-size: 12px;
+  padding: 7px 10px 7px 10px;
+  border: ${({ type }) =>
+    type === 1 ? "1px solid #7AA7C7" : "1px solid #0A95FF"};
+  border-radius: 3px;
+  color: ${({ type }) => (type === 1 ? "#54759D" : "white")};
+  background-color: ${({ type }) => (type === 1 ? "#E1ECF4" : "#0A95FF")};
+`;
+
+const StyledMenu = styled.div`
+  visibility: ${({ isFold }) => (isFold ? "visible" : "hidden")};
+  position: relative;
+  top: 1px;
+  left: -173px;
+  width: 240px;
+  height: 300px;
+  background-color: white;
+  border: 1px solid #e3e6e8;
 `;
 
 function Navigation() {
   // const [isLogin, setIsLogin] = useState(false);
-  const [focused, setfocused] = useState(false);
+  const [focused, setFocused] = useState(false);
+  const [isFold, setIsFold] = useState(false);
 
   const handleFocuse = (e) => {
-    setfocused(!focused);
+    setFocused(!focused);
   };
+
+  const handleFold = (e) => {
+    setIsFold(!isFold);
+  };
+
+  useEffect(() => {
+    document.querySelector("body").addEventListener("click", (e) => {
+      if (focused) {
+        handleFocuse();
+      }
+    });
+  }, [focused]);
 
   return (
     <>
       <StyledNav>
-        <img src={menu} alt="menu" />
-        <img src={logo} alt="logo" />
+        <button onClick={handleFold}>
+          <img src={menu} alt="menu" />
+          <div className="layer"></div>
+        </button>
+        <button>
+          <img src={logo} alt="logo" />
+        </button>
         <nav>
-          <Link to={"/"}>About</Link>
-          <Link to={"/"}>Products</Link>
-          <Link to={"/"}>For Teams</Link>
+          <div>
+            <Link to={"/"}>About</Link>
+          </div>
+          <div>
+            <Link to={"/"}>Products</Link>
+          </div>
+          <div>
+            <Link to={"/"}>For Teams</Link>
+          </div>
         </nav>
-        <StyledSearch focused={focused}>
+        <StyledSearch
+          focused={focused}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
           <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
           <input
             type="text"
             placeholder="Search..."
-            onFocus={handleFocuse}
-            onBlur={handleFocuse}
+            onFocus={() => {
+              if (!focused) {
+                handleFocuse();
+              }
+            }}
           ></input>
         </StyledSearch>
+        <StyledButton type={1}>Log in</StyledButton>
+        <StyledButton type={2}>Sign up</StyledButton>
       </StyledNav>
-      <StyledDiv focused={focused}>
-        <div></div>
+      <StyledDiv onClick={(e) => e.stopPropagation()}>
+        <StyledMenu isFold={isFold}></StyledMenu>
+        <StyledHistory focused={focused}>
+          <div>
+            <ul>
+              {searchData.slice(0, 4).map((data, idx) => {
+                return (
+                  <li key={idx}>
+                    <span>{data[0]}</span>
+                    {data[1]}
+                  </li>
+                );
+              })}
+            </ul>
+            <ul>
+              {searchData.slice(4).map((data, idx) => {
+                return (
+                  <li key={idx}>
+                    <span>{data[0]}</span>
+                    {data[1]}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div>
+            <button>ask Questions</button>
+          </div>
+        </StyledHistory>
       </StyledDiv>
     </>
   );
