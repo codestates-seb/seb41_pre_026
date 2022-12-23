02c94a8b1633e7565c41ac427df3ea9ef691cf68
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-23 17:58:08 +0900

    question 엔티티 클래스 작성
    추후 연관관계 매핑이 필요합니다.

--- server/src/main/java/com/codestates/pre/server/question/entity/Questions.java
+++ server/src/main/java/com/codestates/pre/server/question/entity/Questions.java
@@ -1,4 +1,38 @@
 package com.codestates.pre.server.question.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class Questions {
+	@Id @GeneratedValue
+	private long questionId;
+
+	private String title;
+
+	private String problem;
+
+	private String expecting;
+
+	private LocalDateTime createdAt = LocalDateTime.now();
+
+	private LocalDateTime modifiedAt;
+
+	private int score;
+
+	// Todo QuestionTag 연관관계 매핑
+	// private QuestionTag QuestionTag;
+
+	// TODO member 엔티티 연관관계 매핑
+	// private Member member;
+
+	// Todo answer 엔티티 연관관계 매핑
+	// private Answer answer;
 }
