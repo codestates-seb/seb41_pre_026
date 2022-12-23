3a36d24b5b6978da768fe3459c1802bfb4ace9c5
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-23 18:17:19 +0900

    rename : Questions 엔티티 이름 변경

--- server/src/main/java/com/codestates/pre/server/question/entity/Question.java
+++ server/src/main/java/com/codestates/pre/server/question/entity/Question.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 
 @Entity
 @Getter @Setter
-public class Questions {
+public class Question {
 	@Id @GeneratedValue
 	private long questionId;
 
--- server/src/main/java/com/codestates/pre/server/question/mapper/QuestionMapper.java
+++ server/src/main/java/com/codestates/pre/server/question/mapper/QuestionMapper.java
@@ -5,11 +5,11 @@ import org.mapstruct.Mapper;
 import com.codestates.pre.server.question.dto.QuestionPatchDto;
 import com.codestates.pre.server.question.dto.QuestionPostDto;
 import com.codestates.pre.server.question.dto.QuestionResponseDto;
-import com.codestates.pre.server.question.entity.Questions;
+import com.codestates.pre.server.question.entity.Question;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
-	Questions questionPostDtoToQuestion(QuestionPostDto questionPostDto);
-	Questions questionPatchDtoToQuestion(QuestionPatchDto questionPatchDto);
-	QuestionResponseDto questionToQuestionResponseDto(Questions questions);
+	Question questionPostDtoToQuestion(QuestionPostDto questionPostDto);
+	Question questionPatchDtoToQuestion(QuestionPatchDto questionPatchDto);
+	QuestionResponseDto questionToQuestionResponseDto(Question question);
 }
