dc4db528496a6eb4ba77aaa459a2d3b75ef00dc5
TaeHeyeon_Kim <thk98k@gmail.com>
2022-12-25 22:51:35 +0900

    feat : QuestionRepository(JPA) 구현

---
+++ server/src/main/java/com/codestates/pre/server/respository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.codestates.pre.server.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codestates.pre.server.question.entity.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
