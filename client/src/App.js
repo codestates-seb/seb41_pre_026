484cf65a4023beacd18f8739be46f9bc8e72465e
lmimoh <dlalsgns9039@gmail.com>
2022-12-26 17:14:50 +0900

    style : default css setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,17 @@
 import Questions from "./Pages/Questions";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
 import Navigation from "./Components/Share/Navigation";
 import SideBar from "./Components/Share/SideBar";
-
-const StyledBody = createGlobalStyle`
-  * {
-    font-family: 'Noto Sans KR', sans-serif;
-  }
-  
-  body {
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
-    box-sizing: border-box;
-  }
-`;
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
-      <StyledBody />
       <Navigation />
       <SideBar />
       <Routes>
-        <Route path={"/"} element={<Questions />} />
+        <Route path={"/1"} element={<Questions />} />
+        <Route path={"/"} element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
