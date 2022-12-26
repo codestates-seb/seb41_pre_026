1da9057a4953f2d312954dc7a1d955653a03636a
gimhae-person <hyeino3o@gmail.com>
2022-12-26 23:23:34 +0900

    comment : Advanced 기능에 대한 주석 추가

--- server/src/main/java/com/codestates/pre/server/member/dto/MemberResponseDto.java
+++ server/src/main/java/com/codestates/pre/server/member/dto/MemberResponseDto.java
@@ -1,5 +1,14 @@
 package com.codestates.pre.server.member.dto;
 
+import java.beans.Transient;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codestates.pre.server.answer.entity.Answer;
+import com.codestates.pre.server.answer.repository.AnswerRepository;
+import com.codestates.pre.server.question.entity.Question;
+import com.codestates.pre.server.respository.QuestionRepository;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +21,12 @@ public class MemberResponseDto {
 	private String password;  // 포함되는게 맞는지 모르겟삼
 
 	// private String image;
+
+	/*
+	private List<Question> questions;
+
+	private List<Answer> answers;
+
+	✏️Advanced~
+	 */
 }
