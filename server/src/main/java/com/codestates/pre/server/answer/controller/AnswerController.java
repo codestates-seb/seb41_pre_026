f6a76a98b69ef9fc0fa0746c5b53aa754b4956bb
jinhuiju <jinhuizu@gmail.com>
2022-12-26 23:18:54 +0900

    feat : AnswerController 수정

--- server/src/main/java/com/codestates/pre/server/answer/controller/AnswerController.java
+++ server/src/main/java/com/codestates/pre/server/answer/controller/AnswerController.java
@@ -8,6 +8,8 @@ import com.codestates.pre.server.answer.entity.Answer;
 import com.codestates.pre.server.answer.mapper.AnswerMapper;
 import com.codestates.pre.server.answer.service.AnswerService;
 import com.codestates.pre.server.dto.SingleResponseDto;
+import com.codestates.pre.server.member.entity.Member;
+import com.codestates.pre.server.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/answers")
@@ -23,29 +24,29 @@ import java.net.URI;
 @CrossOrigin
 public class AnswerController {
     private final AnswerService answerService;
-
+    private final MemberService memberService;
     private final AnswerMapper mapper;
 
 
-    public AnswerController(AnswerService answerService, AnswerMapper mapper) {
+    public AnswerController(AnswerService answerService, MemberService memberService, AnswerMapper mapper) {
         this.answerService = answerService;
+        this.memberService = memberService;
         this.mapper = mapper;
     }
 
 
     // question의 id에 맞는 답변을 등록
-    @PostMapping("/{question-id}/add") // uri : /answer/1/add
-    public ResponseEntity postAnswer(@PathVariable("question-id") @Positive long questionId,
-                                    @Valid @RequestBody AnswerPostDto requestBody ) {
+    @PostMapping
+    public ResponseEntity postAnswer(@Valid @RequestBody AnswerPostDto answerPostDto ) {
 
-        Answer answer = mapper.answerPostDtoToAnswer(requestBody); // 요청 바디 > dto > 엔티티
-        answerService.createAnswer(answer); // 비즈니스 로직 거침
+        Answer answer = answerService.createAnswer(mapper.answerPostDtoToAnswer(answerPostDto));
 
-        return new ResponseEntity(HttpStatus.CREATED); // 응답 제공
+       return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)), HttpStatus.CREATED);
     }
 
     //답변 수정
-    @PatchMapping("/{answer-id}/edit")
+    @PatchMapping("/{answer-id}")
     public ResponseEntity patchAnswer(@PathVariable("answer-id") @Positive long answerId,
                                       @Valid @RequestBody AnswerPatchDto patchDto) {
 
@@ -59,9 +60,23 @@ public class AnswerController {
                 HttpStatus.OK); // 응답 제공
     }
 
+    // 답변 조회
+    @GetMapping("/{answer-id}")
+    public ResponseEntity getAnswer(@PathVariable("answer-id") @Positive Long answerId) {
+        Answer answer = answerService.findAnswer(answerId);
+
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)), HttpStatus.OK);
+    }
+
+    // 답변 전체조회
+
+
+
+
+
 
     //답변 삭제
-    @DeleteMapping("/{answer-id}/delete") //405method not Allowed 에러로 url 수정함
+    @DeleteMapping("/{answer-id}")
     public ResponseEntity deleteAnswer(@PathVariable("answer-id") @Positive long answerId) {
 
         // 로그인이 되어있는지 검증
