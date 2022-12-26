237088e1f8eadac7e64109acd9aab58ed429dc36
gimhae-person <hyeino3o@gmail.com>
2022-12-26 11:22:46 +0900

    fix : ExceptionCode 수정

--- server/src/main/java/com/codestates/pre/server/exception/ExceptionCode.java
+++ server/src/main/java/com/codestates/pre/server/exception/ExceptionCode.java
@@ -6,7 +6,9 @@ public enum ExceptionCode {
 	MEMBER_NOT_FOUND(404, "Member not found"),
 	MEMBER_EXISTS(409, "Member exists"),
 	QUESTION_NOT_FOUND(404, "Question not found"),
-	ANSWER_NOT_FOUND(404, "Answer not found");
+	ANSWER_NOT_FOUND(404, "Answer not found"),
+
+	POST_UNDER_TWENTY(406, "Enter at least 20 characters");
 	//TAG_NOT_FOUND(404, "Tag not found");
 
 	@Getter
