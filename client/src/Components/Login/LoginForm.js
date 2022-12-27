a4a67db7100bcb10428849fc3e45bf4acf4c6e92
lmimoh <dlalsgns9039@gmail.com>
2022-12-27 16:50:46 +0900

    feat : login component initial commit

---
+++ client/src/Components/Login/LoginForm.js
@@ -0,0 +1,5 @@
+function LoginForm() {
+  return <></>;
+}
+
+export default LoginForm;
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,10 +1,16 @@
-
 import styled from "styled-components";
+import LoginForm from "../Components/Login/LoginForm";
 
 const StyledDiv = styled.div``;
 
 function Login() {
-  return <StyledDiv></StyledDiv>;
+  return (
+    <StyledDiv>
+      <img></img>
+      <LoginForm></LoginForm>
+      <p></p>
+    </StyledDiv>
+  );
 }
 
 export default Login;
