a081b731d7afd1a2dfc3bb117c6ff717a4ca446c
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2022-12-28 16:00:14 +0900

    feat : sign component (#65)
    * UI 구현 및 recaptch 추가
    
    * recapcha 및 input handle 함수 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createGlobalStyle } from "styled-components";
@@ -27,10 +26,10 @@ const StyledBody = createGlobalStyle`
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <StyledBody />
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </>
 );
