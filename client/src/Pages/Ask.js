b3ca3a722d7c095570db0c491628a3151f9fde51
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2022-12-29 12:13:45 +0900

    feat : Ask component 구현 (#75)
    * feat : Share/Button styled button 총 4 개 구현(hover 포함)
    
    * feat : Pages/Ask.js initial commit
    
    * feat : merge commit
    
    * feat : Ask/Title & Ask/Help 컴포넌트 구현
    
    * design : Ask/Titls.js css 수정
    
    * feat : Pages/Ask 1차 목업 구현
    
    * feat : focus되면 Help 컴포넌트 보여지게 하는 기능 추가
    
    * feat : focus, disabled 기능 구현
    
    * feat : block & disabled 기능 수정 및 보완
    
    * feat : Review & Discard 버튼 기능 1차적  구현 및 disabled & Next 버튼 기능 수정
    
    * design : Ask/Header.js div width 변경
    
    * fix : discard 버튼 활성화/비활성화 기능 수정
    
    * design : Ask/Tags 디자인 및 Ask 컴포넌트 내 input 창 디자인 수정
    
    * refactor : 컴포넌트 비활성화/활성화 조건 수정
    
    * feat : Ask/Tags input 활성화 조건 수정
    
    * feat : input placeholder 조건부 출력되게 수정
    
    * feat : focus된 컴포넌트 scroll 기능 구현 및 각 컴포넌트 focus state 값 변경
    
    * feat : Ask/Tags 활성화 조건 수정
    
    * refactor : placeholder 조건부 렌더링 코드 리팩토링
    
    * feat : focus 기능 구현
    
    * feat : focus 기능 전체 수정

---
+++ client/src/Pages/Ask.js
@@ -0,0 +1,152 @@
+import styled from "styled-components";
+import Header from "../Components/Ask/Header";
+import Title from "../Components/Ask/Title";
+import Problem from "../Components/Ask/Problem";
+import Expect from "../Components/Ask/Expect";
+import Tags from "../Components/Ask/Tags";
+import { StyledBlueBtn, StyledTransRedBtn } from "../Components/Share/Button";
+import { useEffect, useState, useRef } from "react";
+
+const StyledDiv = styled.div`
+  button:nth-child(2) {
+    margin: 0px 0px 0px 16px;
+  }
+
+  .invisible {
+    display: none;
+  }
+
+  .disabledBtn {
+    cursor: not-allowed;
+    opacity: 0.3;
+  }
+`;
+
+function Ask() {
+  const [focus, setFocus] = useState(0);
+  const [isWritten, setIsWritten] = useState([]);
+  const [title, setTitle] = useState("");
+  const [problem, setProblem] = useState("");
+  const [expect, setExpect] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const compRef = useRef([]);
+
+  useEffect(() => {
+    if (focus > 0) {
+      compRef.current[focus].scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+      setTimeout(() => {
+        compRef.current[focus].focus();
+      }, 200);
+    }
+  }, [focus]);
+
+  const handleFocusChange = (focus) => {
+    setFocus(focus);
+  };
+
+  const handleIsWrittenChange = (el) => {
+    setIsWritten([...isWritten, el]);
+  };
+
+  const handleTitleChange = (title) => {
+    setTitle(title);
+  };
+
+  const handleProblemChange = (problem) => {
+    setProblem(problem);
+  };
+
+  const handleExpectChange = (expect) => {
+    setExpect(expect);
+  };
+
+  const handleTagsChange = (tag) => {
+    setTags(tag);
+  };
+
+  const handleSubmit = () => {
+    console.log("title", title);
+    console.log("problem", problem);
+    console.log("expect", expect);
+    console.log("tags", tags);
+  };
+
+  const handleReset = () => {
+    setTitle("");
+    setProblem("");
+    setExpect("");
+    setTags([]);
+    setFocus(0);
+    setIsWritten([]);
+  };
+
+  return (
+    <main>
+      <Header focus={focus} handleFocusChange={handleFocusChange} />
+      <div>
+        <Title
+          focus={focus}
+          handleFocusChange={handleFocusChange}
+          title={title}
+          handleTitleChange={handleTitleChange}
+          isWritten={isWritten}
+          handleIsWrittenChange={handleIsWrittenChange}
+          compRef={compRef}
+        />
+      </div>
+      <div>
+        <Problem
+          focus={focus}
+          handleFocusChange={handleFocusChange}
+          problem={problem}
+          handleProblemChange={handleProblemChange}
+          isWritten={isWritten}
+          handleIsWrittenChange={handleIsWrittenChange}
+          compRef={compRef}
+        />
+      </div>
+      <div>
+        <Expect
+          focus={focus}
+          handleFocusChange={handleFocusChange}
+          expect={expect}
+          handleExpectChange={handleExpectChange}
+          isWritten={isWritten}
+          handleIsWrittenChange={handleIsWrittenChange}
+          compRef={compRef}
+        />
+      </div>
+      <div>
+        <Tags
+          focus={focus}
+          handleFocusChange={handleFocusChange}
+          tags={tags}
+          handleTagsChange={handleTagsChange}
+          isWritten={isWritten}
+          handleIsWrittenChange={handleIsWrittenChange}
+          compRef={compRef}
+        />
+      </div>
+      <StyledDiv ref={(el) => (compRef.current[4] = el)}>
+        <StyledBlueBtn
+          className={focus !== 4 ? "disabledBtn" : ""}
+          onClick={handleSubmit}
+        >
+          Review your question
+        </StyledBlueBtn>
+        <StyledTransRedBtn
+          className={title.length < 15 ? "disabledBtn" : ""}
+          onClick={handleReset}
+        >
+          Discard draft
+        </StyledTransRedBtn>
+      </StyledDiv>
+    </main>
+  );
+}
+
+export default Ask;
