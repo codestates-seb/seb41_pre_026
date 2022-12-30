9472aa8c6d5efdb94320a275aa61a33cc72b4434
lmimoh <dlalsgns9039@gmail.com>
2022-12-30 16:11:11 +0900

    feat : login component API 추가

--- client/src/Components/Login/LoginForm.js
+++ client/src/Components/Login/LoginForm.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { StyledBlueBtn } from "../Share/Button";
 import LoginInput from "./LoginInput";
@@ -11,6 +12,7 @@ const StyledForm = styled.div`
   border-radius: 5px;
   padding: 24px;
   box-shadow: 0px 7px 35px 18px #dedfdf, 1px 2px 1px 1px #dedfdf;
+
   label {
     display: block;
     font-size: 15px;
@@ -24,14 +26,43 @@ const StyledForm = styled.div`
     padding: 8px;
     font-weight: 400;
   }
+
+  span {
+    font-size: 12px;
+    color: red;
+    margin: 0px 0px 0px 3px;
+  }
 `;
 
 function LoginForm({ handleLogin }) {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleId = (e) => setId(e.target.value);
+  const handlePw = (e) => setPw(e.target.value);
+  const handleSubmit = () => {
+    const idRegExp =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (!idRegExp.test(id)) {
+      console.log("e");
+    }
+  };
+
   return (
     <StyledForm>
-      <LoginInput label="Email" />
-      <LoginInput label="Password" />
-      <StyledBlueBtn onClick={handleLogin}>Log in</StyledBlueBtn>
+      <LoginInput
+        value={id}
+        handleId={handleId}
+        place="aaa@bbb.ccc"
+        label="Email"
+      />
+      <LoginInput
+        value={pw}
+        handlePw={handlePw}
+        place="Write.."
+        label="Password"
+      />
+      <StyledBlueBtn onClick={handleSubmit}>Log in</StyledBlueBtn>
     </StyledForm>
   );
 }
--- client/src/Components/Login/LoginInput.js
+++ client/src/Components/Login/LoginInput.js
@@ -4,17 +4,22 @@ import styled from "styled-components";
 const StyledInput = styled.input`
   width: 100%;
   height: 28px;
-  border: 1px solid#babfc4;
   border-radius: 3px;
-  margin-bottom: 10px;
   border: 1px solid #babfc4;
+  padding: 0px 0px 3px 0px;
+
   :focus {
     outline: none;
     border: 1px solid #6bbbf7;
     box-shadow: 0px 0px 4px 4px #d3e5f2;
   }
+  ::placeholder {
+    font-size: 11px;
+    padding: 0px 0px 0px 5px;
+    line-height: 2;
+  }
 `;
-function LoginInput({ label }) {
+function LoginInput({ label, place, handleId, handlePw }) {
   const [isFocused, setIsFocused] = useState(false);
   return (
     <div>
@@ -23,9 +28,12 @@ function LoginInput({ label }) {
         isFocused={isFocused}
         type="text"
         id={label}
+        placeholder={place ? place : null}
         onFocus={() => setIsFocused(!isFocused)}
         onBlur={() => setIsFocused(!isFocused)}
+        onChange={label === "Email" ? handleId : handlePw}
       ></StyledInput>
+      <span>{label} is not valid</span>
     </div>
   );
 }
