5ec6dda0c14ee9117a997497d56930f3a2fc0295
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-01-02 15:33:28 +0900

    Revert "feat : ask component 구현 (#98)" (#99)
    This reverts commit f2297df65bf2c8741699e1614afb6e27d93d3ba8.

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createGlobalStyle } from "styled-components";
-import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
-
-const StyledGlobal = createGlobalStyle`
-  * {
-    font-family: 'Noto Sans KR', sans-serif;
-    box-sizing: border-box;
-  }
-
-  html {
-    height : 100%;
-  }
-  
-  body {
-    height : 100%;
-    margin: 0px 0px 0px 0px;
-    padding: 0px 0px 0px 0px;
-
-    #root {
-      height : 100%;
-    }
-  }
-`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <StyledGlobal />
-    <CookiesProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CookiesProvider>
-  </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
